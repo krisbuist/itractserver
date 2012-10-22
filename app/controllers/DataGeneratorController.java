d3aed425f41c254479cf6fb356d3d837788fb35b
Kris Buist <krisbuist@gmail.com>
2012-10-22 22:26:27 +0200

    Add Stub DataGeneratorController with routes

---
+++ app/controllers/DataGeneratorController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class DataGeneratorController extends Controller {
+
+    public static Result generateOffers(int numberOfOffers)
+    {
+	return TODO;
+    }
+    
+    public static Result generateRequests(int numberOfRequests)
+    {
+	return TODO;
+    }
+    
+}

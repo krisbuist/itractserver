9edae95748d5b8e1a0a45d419f2b9e5cbd5499ce
Kris Buist <krisbuist@gmail.com>
2012-10-22 21:24:45 +0200

    Fix Finder HashMap

--- app/models/TripOffer.java
+++ app/models/TripOffer.java
@@ -1,13 +1,12 @@
 package models;
 
-
 import javax.persistence.Entity;
 
 @Entity
 public class TripOffer extends Trip {
 
     private static final long serialVersionUID = -386674185196292407L;
-    public static Finder<Integer, TripOffer> find = new Finder<>(Integer.class, TripOffer.class);
+    public static Finder<Integer, TripOffer> find = new Finder<Integer, TripOffer>(Integer.class, TripOffer.class);
 
 
     public TripOffer() {

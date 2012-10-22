65a96c1ccbba383bf70cbb548f7688279615f886
Kris Buist <krisbuist@gmail.com>
2012-10-22 15:38:27 +0200

    Implement direction resolving

--- app/googleMapsDirections/Directions.java
+++ app/googleMapsDirections/Directions.java
@@ -1,8 +1,33 @@
 package googleMapsDirections;
 
+import java.util.ArrayList;
+
+import org.codehaus.jackson.JsonNode;
+
 public class Directions {
 
-//    ArrayList<Location> route = new ArrayList<Location>
+    ArrayList<Location> route;
     
+    public Directions()
+    {
+	route = new ArrayList<Location>();
+    }
+    
+    public void addRoutePoint(Location loc)
+    {
+	route.add(loc);
+    }
+    
+    public long calculateDistance()
+    {
+	HttpRequest req = new HttpRequest("");
+	JsonNode result = req.getBody("http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&sensor=false");
+	long distance = Long.parseLong(result.get("routes").findValue("legs").findValue("distance").findValue("value").toString());
+	return distance;
+    }
+    
+    private class Location {
+	
+    }
     
 }

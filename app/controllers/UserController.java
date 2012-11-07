e6f77fce46a1ce98f8334126e228edd9600df649
Kris Buist <krisbuist@gmail.com>
2012-11-07 14:11:46 +0100

    Add new routes

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -4,11 +4,15 @@ import static play.libs.Json.toJson;
 
 import java.util.List;
 
+import models.TripMatch;
+import models.TripOffer;
+import models.TripRequest;
 import models.User;
 import play.data.Form;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
+import flexjson.JSONSerializer;
 
 public class UserController extends Controller {
 
@@ -29,11 +33,13 @@ public class UserController extends Controller {
     @BodyParser.Of(play.mvc.BodyParser.Json.class)
     public static Result newUser() {
 	Form<User> userForm = form(User.class).bindFromRequest();
+	
 	if (userForm.hasErrors()) {
 	    return badRequest();
 	}
 
 	User newUser = userForm.get();
+	newUser.setId(0);
 	newUser.save();
 	response().setContentType("application/json");
 	return status(201, toJson(newUser));
@@ -41,23 +47,22 @@ public class UserController extends Controller {
 
     @BodyParser.Of(play.mvc.BodyParser.Json.class)
     public static Result updateUser(Integer id) {
-	//TODO: Fix update
 	User userToEdit = User.find.byId(id);
-	
+
 	if (userToEdit == null) {
 	    return notFound();
 	}
 
-	Form<User> userForm = form(User.class).fill(userToEdit);
-	
+	Form<User> userForm = form(User.class).bindFromRequest();
+
 	if (userForm.hasErrors()) {
 	    return badRequest();
 	}
-	
+
 	User editedUser = userForm.get();
 	editedUser.setId(userToEdit.getId());
 	editedUser.update();
-	
+
 	return status(200, toJson(editedUser));
     }
 
@@ -69,5 +74,38 @@ public class UserController extends Controller {
 	} else {
 	    return notFound();
 	}
+    }
+
+    public static Result getRequestsByUser(Integer id) {
+	List<TripRequest> requests = TripRequest.find.join("user").where().eq("user.id", id).findList();
+
+	JSONSerializer serializer = new JSONSerializer().exclude("*.class").include("*");
+
+	response().setContentType("application/json");
+	return ok(serializer.serialize(requests));
+    }
+
+    public static Result getOffersByUser(Integer id) {
+
+	List<TripOffer> offers = TripOffer.find.join("user").where().eq("user.id", id).findList();
+
+	JSONSerializer serializer = new JSONSerializer().exclude("class").include("*");
+
+	response().setContentType("application/json");
+	return ok(serializer.serialize(offers));
+    }
+
+    public static Result getMatchesByUser(Integer id) {
+	List<TripMatch> matches = TripMatch.find.join("tripRequest").where().eq("tripRequest.user.id", id).findList();
+	
+	JSONSerializer serializer = new JSONSerializer().exclude("class").include("*");
+
+	response().setContentType("application/json");
+	return ok(serializer.serialize(matches));
+    }
+    
+    public static Result login()
+    {
+	return TODO;
     }
 }

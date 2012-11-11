b301c1baee92a737575f053b98aa24a83869da75
Marthyn Elzes <marthynelzes@gmail.com>
2012-11-11 19:05:51 +0100

    fix for deepserialisation error

--- app/models/TripOffer.java
+++ app/models/TripOffer.java
@@ -1,22 +1,28 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 @Entity
 public class TripOffer extends Trip {
 
-    private static final long serialVersionUID = -386674185196292407L;
-    public static Finder<Integer, TripOffer> find = new Finder<Integer, TripOffer>(Integer.class, TripOffer.class);
+	private static final long serialVersionUID = -386674185196292407L;
+	public static Finder<Integer, TripOffer> find = new Finder<Integer, TripOffer>(
+			Integer.class, TripOffer.class);
 
-    public TripOffer() {
-        super();
-    }
-    
-    public List<TripMatch> getMatches()
-    {
-	return TripMatch.find.where().eq("tripOffer.id", this.getId()).findList();
-    }
+	@OneToMany
+	private List<TripMatch> matches = new ArrayList<TripMatch>();
+
+	public TripOffer() {
+		super();
+	}
+
+	public List<TripMatch> getMatches() {
+		return TripMatch.find.where().eq("tripOffer.id", this.getId())
+				.findList();
+	}
 
 }

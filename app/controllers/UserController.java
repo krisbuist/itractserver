d8f986f3c59f3948676c01400017dcbe1c1df130
Marthyn Elzes <marthynelzes@gmail.com>
2012-11-12 18:18:36 +0100

    fixed error in UserController.java with search user

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -146,16 +146,15 @@ public class UserController extends Controller {
     @With(BasicAuthAction.class)
     public static Result searchUserByName(String name)
     {
-	List<User> users = User.find.where().like("email", "%" + name + "%").findList();
+    	List<User> users = User.find.where().like("email", "%" + name + "%").findList();
 
-        User user = activeUser();
         response().setHeader("Access-Control-Allow-Headers", "Authorization, content-type");
         response().setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         response().setHeader("Access-Control-Allow-Origin", "*");
         response().setHeader("Access-Control-Allow-Credentials", "true");
         response().setHeader("Access-Control-Request-Headers", "origin, content-type, accept, authorization");
         response().setHeader("Access-Control-Max-Age", "60");
-    	return ok(toJson(user));
+    	return ok(toJson(users));
     }
 
     @With(BasicAuthAction.class)
